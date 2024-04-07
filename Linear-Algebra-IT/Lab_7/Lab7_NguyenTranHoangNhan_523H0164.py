7c8411bc4898f631c1e5341492ebad3292ecd77e
Anakonkai <nguyentranhoangnhan18@gmail.com>
2024-04-07 14:21:40 +0700

    da hoan thanh

---
+++ Linear-Algebra-IT/Lab_7/Lab7_NguyenTranHoangNhan_523H0164.py
@@ -0,0 +1,27 @@
+import sympy as sp
+import numpy as np
+
+def exercise1():
+    av1 = np.array([1,2,3,4])
+    av2 = np.array([-1,0,1,3])
+    av3 = np.array([0,5,-6,8])
+    aw = np.array([3,-6,17,11])
+
+    a = np.column_stack(av1,av2,av3)
+
+    bv1 = np.array([1,1,2,2])
+    bv2 = np.array([2,3,5,6])
+    bv3 = np.array([2,-1,3,6])
+    bw = np.array([0,5,3,0])
+
+    cv1 = np.array([1,1,2,2])
+    cv2 = np.array([2,3,5,6])
+    cv3 = np.array([2,-1,3,6])
+    cw = np.array([-1,6,1,-4])
+
+    dv1 = np.array([1,2,3,4])
+    dv2 = np.array([-1.0,1,3])
+    dv3 = np.array([0,5,-6,8])
+    dv4 = np.array([1,15,-12,8])
+    dw = np.array([0,-6,17,11])
+
--- Linear-Algebra-IT/MidTerm/Midterm.py
+++ Linear-Algebra-IT/MidTerm/Midterm.py
@@ -1,20 +1,18 @@
 import numpy as np
-
+import sympy as sp
 # Declaration
 # Matrix A
 rows = 10
 cols = 10
-a_matrix = np.arange(1, rows * cols + 1).reshape(rows,cols) # Create a 1D array with sequential numbers
-
+a_matrix = np.random.randint(1, rows * cols + 1,(rows,cols))
 # Matrix B
 rows = 2
 cols = 10
-b_matrix = np.arange(1, rows * cols + 1).reshape(rows,cols) # Create a 1D array with sequential numbers
-
+b_matrix = np.random.randint(1, rows * cols + 1,(rows,cols))
 # Matrix C
 rows = 10
 cols = 2
-c_matrix = np.arange(1, rows * cols + 1).reshape(rows,cols) # Create a 1D array with sequential numbers
+c_matrix = np.random.randint(1, rows * cols + 1,(rows,cols))
 
 
 # in ma tran do ra luon 
@@ -50,44 +48,19 @@ def d():
     print(result_matrix)
 
 def e():
-    def is_prime(n):
-        if n <= 1:
-            return False
-        for i in range(2, int(np.sqrt(n)) + 1):
-            if n % i == 0:
-                return False
-        return True
-
-    prime_numbers = np.array([num for row in a_matrix for num in row if is_prime(num)])
+    prime_numbers = np.array([num for row in a_matrix for num in row if sp.isprime(num)])
     print(prime_numbers)
 
 def f():
     d_matrix = np.matmul(c_matrix,b_matrix)
-
-    # Reverse elements in the odd rows of the matrix D
     for i in range(len(d_matrix)):
         if i % 2 == 0:
             d_matrix[i] = np.flip(d_matrix[i])
-
-    # Print the resultant matrix
     print(d_matrix)
 
 def g():
-    def is_prime(n):
-        if n <= 1:
-            return False
-        for i in range(2, int(np.sqrt(n)) + 1):
-            if n % i == 0:
-                return False
-        return True
-
-    # Find the maximum number of prime numbers in any row
-    max_prime_count = max(sum(is_prime(num) for num in row) for row in a_matrix)
-
-    # Find all rows with the maximum number of prime numbers
-    rows_with_max_primes = [row for row in a_matrix if sum(is_prime(num) for num in row) == max_prime_count]
-
-    # Print the rows with the maximum number of prime numbers
+    max_prime_count = max(sum(sp.isprime(num) for num in row) for row in a_matrix)
+    rows_with_max_primes = [row for row in a_matrix if sum(sp.isprime(num) for num in row) == max_prime_count]
     for row in rows_with_max_primes:
         print(row)
 
@@ -102,37 +75,30 @@ def h():
             else:
                 current_length = 0
         return max_length
-
-    # Find the length of the longest contiguous odd numbers sequence in each row
     max_odd_sequence_lengths = [longest_odd_sequence(row) for row in a_matrix]
-
-    # Find the maximum length of the contiguous odd numbers sequence among all rows
     max_length = max(max_odd_sequence_lengths)
-
-    # Find the rows with the maximum length of the contiguous odd numbers sequence
     rows_with_max_length = [row for row, length in zip(a_matrix, max_odd_sequence_lengths) if length == max_length]
-
-    # Print the rows with the longest contiguous odd numbers sequence
     for row in rows_with_max_length:
         print(row)
 
 
 
-printMatrix()
 # Call the function
+printMatrix()
+print()
 print("Task 1a:")
 a()
-print("Task 1b:")
+print("\nTask 1b:")
 b()
-print("Task 1c:")
+print("\nTask 1c:")
 c()
-print("Task 1d:")
+print("\nTask 1d:")
 d()
-print("Task 1e:")
+print("\nTask 1e:")
 e()
-print("Task 1f:")
+print("\nTask 1f:")
 f()
-print("Task 1g:")
+print("\nTask 1g:")
 g()
-print("Task 1h:")
+print("\nTask 1h:")
 h()
