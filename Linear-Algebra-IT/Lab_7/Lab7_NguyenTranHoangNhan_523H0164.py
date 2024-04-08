40e84c95cd1ddcdafda28ab1b34e6eaa4dc1eb8a
Anakonkai <nguyentranhoangnhan18@gmail.com>
2024-04-08 08:25:24 +0700

    chua lam lab 7, hoan thanh xong bai giua ki

--- Linear-Algebra-IT/Lab_7/Lab7_NguyenTranHoangNhan_523H0164.py
+++ Linear-Algebra-IT/Lab_7/Lab7_NguyenTranHoangNhan_523H0164.py
@@ -7,17 +7,22 @@ def exercise1():
     av3 = np.array([0,5,-6,8])
     aw = np.array([3,-6,17,11])
 
-    a = np.column_stack(av1,av2,av3)
+    a = np.column_stack(av1,av2,av3,aw)
+    a = a.np.linalg.
 
     bv1 = np.array([1,1,2,2])
     bv2 = np.array([2,3,5,6])
     bv3 = np.array([2,-1,3,6])
     bw = np.array([0,5,3,0])
 
+    b = np.column_stack(bv1,bv2,bv3,bw)
+
     cv1 = np.array([1,1,2,2])
     cv2 = np.array([2,3,5,6])
     cv3 = np.array([2,-1,3,6])
     cw = np.array([-1,6,1,-4])
+
+    c = np.column_stack(cv1,cv2,cv3,cw)
 
     dv1 = np.array([1,2,3,4])
     dv2 = np.array([-1.0,1,3])
