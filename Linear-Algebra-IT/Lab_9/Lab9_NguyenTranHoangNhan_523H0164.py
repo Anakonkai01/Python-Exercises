945cf2e02cf1b559cb35c14330e302a567b90eba
Anakonkai <nguyentranhoangnhan18@gmail.com>
2024-04-25 14:04:06 +0700

    da chinh sua lai lab 9

--- Linear-Algebra-IT/Lab_9/Lab9_NguyenTranHoangNhan_523H0164.py
+++ Linear-Algebra-IT/Lab_9/Lab9_NguyenTranHoangNhan_523H0164.py
@@ -42,10 +42,10 @@ def exercise7():
     result_d = np.dot(S_minus_1_1, v)
 
     # Print the results
-    print("Result (a) S2,2 with λ = 2 and µ = 2:", result_a)
-    print("Result (b) S0.5,0.5 with λ = 0.5 and µ = 0.5:", result_b)
-    print("Result (c) S1,-1 with λ = 1 and µ = -1:", result_c)
-    print("Result (d) S-1,1 with λ = -1 and µ = 1:", result_d)
+    print("Result (a) S2,2 with λ = 2 and µ = 2:", result_a," Scale with 200% compare to the original")
+    print("Result (b) S0.5,0.5 with λ = 0.5 and µ = 0.5:", result_b," Scale with 50% compare to the original")
+    print("Result (c) S1,-1 with λ = 1 and µ = -1:", result_c," Symetrix across the x-axis")
+    print("Result (d) S-1,1 with λ = -1 and µ = 1:", result_d,"Symetrix across y-axis")
 
 def exercise8():
     print()
@@ -215,4 +215,12 @@ def exercise11():
     plt.title("Implement task 11b")
     plt.grid(True)
     plt.show()
+
+
+
+exercise6()
+exercise7()
+exercise8()
+exercise9()
+exercise10()
 exercise11()
