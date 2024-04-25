f74dc9b00ab041b9dcbcaaa1c6ecd1d779d81cdd
Anakonkai <nguyentranhoangnhan18@gmail.com>
2024-04-25 13:51:59 +0700

    hoan thanh lab 9

--- Linear-Algebra-IT/Lab_9/exercise8/testChatGpt.py
+++ Linear-Algebra-IT/Lab_9/exercise8/testChatGpt.py
@@ -1,18 +1,14 @@
 import numpy as np
 
-# Define the rotation matrices
 def rotation_matrix(theta):
     return np.array([[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]])
 
-# (a) Describe geometrically the action of Rθ with θ = π
 theta_a = np.pi
 R_pi = rotation_matrix(theta_a)
 
-# (b) Describe geometrically the action of Rθ with θ = π/3
 theta_b = np.pi / 3
 R_pi_over_3 = rotation_matrix(theta_b)
 
-# Print the rotation matrices
 print("Rotation matrix Rπ:")
 print(R_pi)
 print("\nRotation matrix Rπ/3:")
